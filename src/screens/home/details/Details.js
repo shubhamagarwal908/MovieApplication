505206163818e1fbfdf34e43c6ba9fb2d9829fbf
EvilRAT Technologies <evilrattechnologies@gmail.com>
2022-09-04 15:53:44 +0530

    Checkpoint5

---
+++ src/screens/home/details/Details.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Details.css';
+
+export class Details extends Component {
+  render() {
+    return (
+      <div>Details</div>
+    )
+  }
+}
+
+export default Details
